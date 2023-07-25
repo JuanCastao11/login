c98612b58c9ba96508998758f5ab4dab13ca9a99
Juan Manuel Castaño <juanmanuelcastano11b@gmail.com>
2023-07-25 17:08:53 -0500

    Encripte contraseña

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,37 @@
 import User from '../models/user.models.js';
+import bcrypt from 'bcryptjs';
+import { createTokenAccess } from '../libs/jwt.js';
 
 
-export const register = (req, res) => res.send("Register");
+
+export const register = async (req, res) => {
+    const {email, password, username} = req.body;
+
+  //  console.log(email, password, username);
+  //  res.send('Registrado');
+  try{
+    const passwordHash = await bcrypt.hash(password,10)
+    const newUser = new User({
+        username,
+        email,
+        password:passwordHash
+
+    });
+
+    const userSaved = await newUser.save();
+    const token = await createTokenAcces({ id: userSaved.id});
+    res.cookie('token', token)
+    res.status(201).json({
+        id: userSaved.id,
+        username: userSaved.username,
+        email: userSaved.email
+    })
+  }catch (error){
+    res.status(500).json({message: error.message});
+  }
+};
+
+
+
+
 export const login = (req, res) => res.send("login");
