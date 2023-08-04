925070e51867127096a2bf1d15f1b018b8dc4cb0
JuanCastao11 <juanmanuelcastano11b@gmail.com>
2023-08-04 10:01:07 -0500

    Importe cors en el backend del crud

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,4 +1,6 @@
 import { useForm } from 'react-hook-form';
+import { registerRequest } from '../api/auth';
+
 
 
 function RegisterPage() {
@@ -8,11 +10,16 @@ function RegisterPage() {
     <div className='bg-zinc-800 ma-10 max-w-md p-5 rounded-md'>
         <form onSubmit={handleSubmit (async(values) =>{
             console.log(values);
+            const res = await registerRequest(values)
+            console.log(res);
         })}>
-            <input  type="text" {...register('username', {required:true})} placeholder='username'/>
-            <input  type="email" {...register('email', {required:true})} placeholder='email'/>
-            <input  type="password" {...register('password', {required:true})} placeholder='password'/>
-            <button type="submit" className="rounded-full">Register</button>
+            <input  type="text" {...register('username', {required:true})} placeholder='username'
+             className='w-full bg-zinc-600 text-white px-4 rounded-md my-2 py-5'/>
+            <input  type="email" {...register('email', {required:true})} placeholder='email'
+              className='w-full bg-zinc-600 text-white px-4 rounded-md my-2 py-5'/>
+            <input  type="password" {...register('password', {required:true})} placeholder='password'
+              className='w-full bg-zinc-600 text-white px-4 rounded-md my-2 py-5'/>
+            <button type="submit" className="bg-blue-500 hover:bg-slate-700 text-white font-bold py-3 px-4 rounded ">Register</button>
         </form>   
 
     </div>
